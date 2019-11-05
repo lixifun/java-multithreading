0faae9211269216fcc5d8e58ec897b03221ff027
lixifun <lixifun@qq.com>
2019-11-05 20:24:20 +0800

    ThreadLocal

---
+++ src/t22/ThreadLocal2.java
@@ -0,0 +1,36 @@
+package t22;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 线程局部变量
+ */
+public class ThreadLocal2 {
+
+    static ThreadLocal<Person> tl = new ThreadLocal<>();
+
+    public static void main(String[] args) {
+
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(tl.get());
+        }).start();
+
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            tl.set(new Person());
+        }).start();
+    }
+
+    static class Person {
+        String name = "zhangsan";
+    }
+}
